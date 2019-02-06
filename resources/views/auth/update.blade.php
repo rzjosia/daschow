21b1585d448cfa7a3fe6cb8c90d612b3d590023b
josia <rzjosia@bitbucket.org>
2019-02-06 19:29:52 +0100

    personnalisation couleurs

--- resources/views/auth/update.blade.php
+++ resources/views/auth/update.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <div class="card text-white bg-dark">
                     <div class="card-header">
                         Profil
                     </div>
